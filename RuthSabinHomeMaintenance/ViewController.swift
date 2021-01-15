bdd75cd567ac39eb02f596945b84045ea1766917
NateJose123 <josen@berea.edu>
2021-01-15 15:10:34 -0600

    Initial Commit

---
+++ RuthSabinHomeMaintenance/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RuthSabinHomeMaintenance
+//
+//  Created by Nathan Jose on 1/15/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ RuthSabinHomeMaintenanceTests/RuthSabinHomeMaintenanceTests.swift
@@ -0,0 +1,33 @@
+//
+//  RuthSabinHomeMaintenanceTests.swift
+//  RuthSabinHomeMaintenanceTests
+//
+//  Created by Nathan Jose on 1/15/21.
+//
+
+import XCTest
+@testable import RuthSabinHomeMaintenance
+
+class RuthSabinHomeMaintenanceTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
